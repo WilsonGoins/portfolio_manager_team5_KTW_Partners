02e3b3bf02d06b2bf1620780ccac22eaee900cfa
kbaggott1 <kevin.baggott1@gmail.com>
2026-07-27 15:56:34 -0400

    added portfolio_values table

--- server/db_items/__init__.py
+++ server/db_items/__init__.py
@@ -1,5 +1,6 @@
 from .holding import Holding
 from .transaction import Transaction
 from .db_item import ADataBaseItem
+from .portfolio_value import PortfolioValue
 
-__all__ = ["Holding", "Transaction", "ADataBaseItem"]
+__all__ = ["Holding", "Transaction", "ADataBaseItem", "PortfolioValue"]
---
+++ server/db_items/portfolio_value.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+from .db_item import ADataBaseItem
+from typing import Self
+
+
+class PortfolioValue(ADataBaseItem):
+    def __init__(self, p_date: datetime, value: float):
+        self.p_date = p_date
+        self.value = value
+
+    @classmethod
+    def from_dict(cls, class_as_dict: dict) -> Self:
+        return cls(p_date=class_as_dict["p_date"],
+                   value=class_as_dict["value"])
+
+        def to_dict(self) -> dict:
+            return {
+                "p_date": self.p_date,
+                "value": self.value
+            }
