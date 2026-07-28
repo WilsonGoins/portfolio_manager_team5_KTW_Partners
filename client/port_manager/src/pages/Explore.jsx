10b8542204aea7275da05f21a925e8e86d871643
kbaggott1 <kevin.baggott1@gmail.com>
2026-07-28 16:15:59 -0400

    work on explore

--- client/port_manager/src/pages/Explore.jsx
+++ client/port_manager/src/pages/Explore.jsx
@@ -1,3 +1,15 @@
+import {ExploreSecurities} from "../components/explore/ExploreSecurities";
+import {ExploreNews} from "../components/explore/ExploreNews";
+
 export function Explore() {
-  return <h2>Explore Page</h2>
+  return (
+    <>
+      <div className="explore-page">
+        <div className="explore-main-content">
+          <ExploreSecurities />
+          <ExploreNews />
+        </div>
+      </div>
+    </>
+  )
 }
