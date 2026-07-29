19ccfcf48020b6655dee10bfdcd8918765705dc2
Wilson Goins <wilsonfgoins@gmail.com>
2026-07-29 15:12:43 -0400

    fixed navbar. deleted holdings.jsx

--- client/port_manager/src/components/Navbar.jsx
+++ client/port_manager/src/components/Navbar.jsx
@@ -1,19 +1,18 @@
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import "./Navbar.css";
 
 export function Navbar() {
   return (
     <header className="navbar-container">
-      <div className="navbar-brand">
+      <Link className="navbar-brand" to="/">
         <div className="brand-icon">K</div>
         <span className="brand-name">KTW Partners</span>
-      </div>
+      </Link>
 
       <nav className="navbar-links">
         <NavLink to="/" end>Overview</NavLink>
-        <NavLink to="/holdings">Holdings</NavLink>
-        <NavLink to="/transactions">Transactions</NavLink>
         <NavLink to="/explore">Explore</NavLink>
+        <NavLink to="/transactions">Transactions</NavLink>
       </nav>
     </header>
   );
